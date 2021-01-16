e34da6ef42b058ac4fd15fd121014451859e9018
omosteven <omosteven123@gmail.com>
2021-01-16 17:18:21 +0100

    Fixed dependencies

--- middlewares/CurrencyRateMiddleware.js
+++ middlewares/CurrencyRateMiddleware.js
@@ -3,18 +3,7 @@ const axios = require("axios").default;
 class CurrencyRateMiddleware {
 
     static async fetchCurrencyRate(base, currency) {
-        // new Promise((reject, resolve) => {
 
-        //     const fetchUrl = `https://api.exchangeratesapi.io/latest?symbols=${currency}&base=${base}`;
-        //     console.log("test", fetchUrl)
-        //     axios.get(fetchUrl).then(resp => {
-        //         console.log("ddd",resp)
-        //         resolve(resp);
-        //     }).catch(err => {
-        //         console.log("hh",err)
-        //         reject(err);
-        //     })
-        // })
         try {
             const fetchUrl = `https://api.exchangeratesapi.io/latest?symbols=${currency}&base=${base}`;
 
